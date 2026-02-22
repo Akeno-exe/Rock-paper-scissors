65a406b803a86663ab164e454015ddcc32efa579
Amarbayar Davaajav <amarbayar117@g.skku.edu>
2026-02-22 18:50:03 +0800

    Revert "Added a few comments to make it more understandable for other developers"
    This reverts commit 283be51383c5142e8a0b1bb487d757b731fc63e6.

--- discordmemebot/memebot.py
+++ discordmemebot/memebot.py
@@ -28,5 +28,5 @@ intents.message_content = True
 
 client = MyClient(intents=intents)
 
-# Paste your discord bot token instead of TOKEN
+# USE THE VARIABLE HERE, NOT THE LONG STRING
 client.run(TOKEN)
